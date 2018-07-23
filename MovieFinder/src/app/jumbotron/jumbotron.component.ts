9054076f9c7c5b97fcfe7c1985ea1fdc655ca945
PeterAsenov22 <peter12345@abv.bg>
2018-07-23 22:29:59 +0300

    Jumbotron component added

---
+++ MovieFinder/src/app/jumbotron/jumbotron.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-jumbotron',
+  templateUrl: './jumbotron.component.html',
+  styleUrls: ['./jumbotron.component.css']
+})
+export class JumbotronComponent {
+}
